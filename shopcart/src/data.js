ce519d3bb6615db001fa00292be7164f9053f820
praveen3541 <144254592+praveen3541@users.noreply.github.com>
2023-10-12 11:40:57 +0530

    Add files via upload

---
+++ shopcart/src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'MacBook',
+        price: 1400,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Old Car',
+        price: 2400,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '3',
+        name: 'Laptop',
+        price: 3000,
+        image: 'https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      },
+    ],
+  };
+  export default data;
+  
